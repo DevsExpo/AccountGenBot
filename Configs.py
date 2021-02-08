117e6b45d55f4a5b521a2255ccd1b28605dc43ec
スタークギャング <StarkGangZ@gmail.com>
2021-02-08 07:27:19 +0530

    Update Configs.py

--- Configs.py
+++ Configs.py
@@ -4,8 +4,8 @@ import os
 class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
     DB_URI = os.environ.get("DATABASE_URL", None)
-    API_HASH = os.environ.get("API_HASH", None)
-    API_ID = int(os.environ.get("APP_ID", 6))
+    API_HASH = os.environ.get("API_HASH", "hfcdgv3656hshs")
+    API_ID = int(os.environ.get("API_ID", 6))
     ACCOUNT_GEN_NAME = os.environ.get("ACCOUNT_GEN_NAME", "Alt-Balaji")
     JTU_ENABLE = os.environ.get("JTU_ENABLE", False)
     CHANNEL_USERNAME = os.environ.get("CHANNEL_USERNAME", None)
