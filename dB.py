86eeb45319d5f286ebf0c8f1d8b76c991d2cdc6a
スタークギャング <StarkGangZ@gmail.com>
2021-03-08 17:02:56 +0530

    Update dB.py

--- dB.py
+++ dB.py
@@ -1,4 +1,4 @@
-#    Copyright (C) Midhun KM 2020-2021
+#    Copyright (C) DEVSEXPO 2020-2021
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
