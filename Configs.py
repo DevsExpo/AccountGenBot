a1739e017be88dea95bdee15b6b69e9d740d9aad
スタークギャング <StarkGangZ@gmail.com>
2021-03-08 16:30:16 +0530

    Update Configs.py

--- Configs.py
+++ Configs.py
@@ -4,6 +4,7 @@ import os
 class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
     DB_URI = os.environ.get("DATABASE_URL", None)
+    MG_DB_URL = os.environ.get("MG_URL", None)
     API_HASH = os.environ.get("API_HASH", "hfcdgv3656hshs")
     API_ID = int(os.environ.get("API_ID", 6))
     ACCOUNT_GEN_NAME = os.environ.get("ACCOUNT_GEN_NAME", "Alt-Balaji")
